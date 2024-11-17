ed44a1fbaae758649291111e9b7281aaa2ff80ba
James <imblazeitjames@gmail.com>
2024-11-17 17:15:45 -0600

    fix: adjust vehicle and pedestrian density values for improved gameplay balance

--- config/client.lua
+++ config/client.lua
@@ -4,8 +4,8 @@ return {
     -- Value of 1.0 represents GTA Online populate rates
     -- Value of 0.0 removes all of that type
     parked = 0.8, -- Density of parked vehicles
-    vehicle = 0.8, -- Density of vehicles
-    randomvehicles = 0.8, -- Density of random vehicles
+    vehicle = 0.65, -- Density of vehicles
+    randomvehicles = 0.55, -- Density of random vehicles
     peds = 0.8, -- Density of random peds (civilians, etc.)
-    scenario = 0.8, -- Density of scenario peds (bikers, gangsters, etc.)
+    scenario = 0.55, -- Density of scenario peds (bikers, gangsters, etc.)
 }
